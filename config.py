42c77a6418996533034fa51e0fe976ada9917e94
Rubén Salas <ruben.sls@outlook.com>
2022-11-24 17:11:41 +0100

    Add config file to fix problem with streamlit cloud

---
+++ config.py
@@ -0,0 +1,12 @@
+#
+#
+#   Config
+#
+#
+
+import os
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+DOWNLOADS_ROOT_DIR = os.getenv("DOWNLOADS_ROOT_DIR", os.path.join(HERE, "downloads"))
+DATA_ROOT_DIR = os.getenv("DATA_ROOT_DIR", os.path.join(HERE, "data"))
